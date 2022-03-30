961f95caaee991a55fef5f10630dafbc7223fc5c
J-DouglasLira <jdouglira@gmail.com>
2022-03-30 19:13:36 -0300

    routes added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
-import Header from "./components/Header";
+import Header from "./Header";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
+const Home = () => {
+  return <h1>Home!</h1>
+}
+
+const Generos = () => {
+  return <h1>Generos!</h1>
+}
 
 function App() {
-  
-  
   return (
-    <div>
-      <Header/>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route  path="/" exact element={<Home/>} />
+          <Route  path="/generos" exact element={<Generos/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
+import { render } from 'react-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
